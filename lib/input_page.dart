1ff37dd00116dc85da9f3f4e16fe0dd5861b9704
Oğuzhan Arslantaş <iamoguzhanarslantas@gmail.com>
2023-02-01 16:39:08 +0300

    layout and constants

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
 
@@ -15,8 +19,70 @@ class _InputPageState extends State<InputPage> {
         centerTitle: true,
         title: const Text('BMI Calculator'),
       ),
-      body: const Center(
-        child: Text('Body'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: const [
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Expanded(
+            child: ReusableCard(
+              colour: activeCardColour,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: const [
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColour,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColour,
+            margin: const EdgeInsets.only(
+              top: 13,
+            ),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color colour;
+  const ReusableCard({super.key, required this.colour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(13),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(8),
       ),
     );
   }
